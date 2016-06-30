a4342a5947744c2883a3686407aee96144187997
el-ROSSO <rosshagino@icloud.com>
2016-06-29 14:24:42 -1000

    ADDED STUFF TO LOTTO
    added button, random #, and fancy trick

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function  lotto() {
+
+
+	var lottoNumber = Math.floor(Math.random()*10000000000000000001);
+
+	document.getElementById('display').innerHTML = "<h1>"+"YOU WIN LUCKY NUMBER: "+lottoNumber+" JUST KIDDiNG >:)";
+
+};
